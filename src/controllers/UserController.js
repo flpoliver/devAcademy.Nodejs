6fc21da0d51929305ed294032c0acac5c45b9cbc
Felipe Oliveira <flpeolivier@gmail.com>
2021-09-14 16:33:43 -0300

    finalizando o CRUD com métodos update e delete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,4 @@
-const users = require('../mocks/users');
+let users = require('../mocks/users');
 
 module.exports = {
   listUsers(req, res) {
@@ -41,4 +41,39 @@ module.exports = {
 
     res.send(200, newUser);
   },
+
+  updateUser(req, res) {
+    let { id } = req.params;
+    const { name } = req.body;
+
+    id = Number(id);
+
+    const userExists = users.find((user) => user.id === id);
+
+    if(!userExists) {
+      return res.send(400, { error: 'User not found' })
+    }
+
+    users = users.map((user) => {
+      if (user.id === id) {
+        return {
+          ...user,
+          name,
+        };
+      }
+
+      return user;
+    });
+
+    res.send(200, { id, name });
+  },
+
+  deleteUser(req, res) {
+    let { id } = req.params;
+    id = Number(id);
+
+    users = users.filter((user) => user.id !== id);
+
+    res.send(200, { deleted: true });
+  }
 };
